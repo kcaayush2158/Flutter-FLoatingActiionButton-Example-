34c7c7058e76d9d65953ce0444db748802d1a807
aayushkc2158 <aayushkc2158@gmail.com>
2019-11-22 04:43:51 +0545

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,31 +6,42 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Raised Buttom",
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(
-          title: AppBar(
-            title: Text("Raised Button"),
-          ),
-        ),
-        body: Container(
-          color: Colors.deepPurple,
-          alignment: Alignment.center,
-          child: Center(
-            child: RaisedButton(
-              child: Text(
-                "RAISED BUTTON",
-                style: TextStyle(color: Colors.white),
-              ),
-              onPressed: () {},
-              splashColor: Colors.lightBlue,
-              color: Colors.lightBlueAccent,
-            ),
+    return MaterialApp(title: "Raised Buttom", home: TextPage());
+  }
+}
+
+class TextPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Raised Button"),
+      ),
+      body: Container(
+        child: Center(
+          child: RaisedButton(
+            color: Colors.redAccent,
+            textColor: Colors.white,
+            onPressed: () {
+              pressMe(context);
+            },
+            child: Text("Press Me"),
           ),
         ),
       ),
     );
+  }
+
+  void pressMe(BuildContext context) {
+    var alert = AlertDialog(
+      title: Text("Alert Dialog"),
+      content: Text("Success..!"),
+    );
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alert;
+        });
   }
 }
